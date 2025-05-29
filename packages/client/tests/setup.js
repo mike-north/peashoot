9c12d48abe285e7f4a167cd77930af0f682b8a64
Mike North <michael.l.north@gmail.com>
2025-05-28 21:00:51 -0700

    remove unused eslint-disable directive

--- packages/client/tests/setup.js
+++ packages/client/tests/setup.js
@@ -7,7 +7,7 @@ Object.defineProperty(window, 'matchMedia', {
 	writable: true,
 	value: vi.fn().mockImplementation((query) => ({
 		matches: false,
-		// eslint-disable-next-line no-undef, @typescript-eslint/no-unsafe-assignment
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 		media: query,
 		onchange: null,
 		addListener: vi.fn(), // deprecated
